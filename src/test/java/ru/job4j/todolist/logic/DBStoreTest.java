348f50cd65e5e67d3b712074d0c778cbdc62cf29
Victor Egorov <qrioflat@gmail.com>
2020-04-17 21:15:08 +0300

    Создать TODO list[#230536]

---
+++ src/test/java/ru/job4j/todolist/logic/DBStoreTest.java
@@ -0,0 +1,23 @@
+package ru.job4j.todolist.logic;
+
+import org.junit.Test;
+import ru.job4j.todolist.models.Item;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+
+public class DBStoreTest {
+
+    private static final String TEST = "Test";
+
+    @Test
+    public void whenAddItemThenTaskInStore() {
+        Store<Item> store = DBStore.getInstance();
+        Item item = store.add(new Item().setDescription(TEST));
+        assertThat(item.getId(), not(0));
+        assertThat(store.findAll().stream().filter(item1 -> item1.getId() == item.getId()).count(), is(1L));
+        store.delete(item);
+    }
+
+}
